eec5b5d93cc38bebb51c650e6dcd135d3a37ae2f
Felix Althaus <felix.althaus@undkonsorten.com>
2021-03-16 16:04:26 +0100

    [FEATURE] Always include TypoScript via code
    This way you don’t have to bother with where your TS for the
    backend module actually comes from.

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('taskqueue', 'Configuration/TypoScript', 'task');
