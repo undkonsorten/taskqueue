238cfcce6ed8da7ef9abb5c0b32ad416bc066e25
Eike Starkmann <eike.starkmann@posteo.de>
2021-11-17 21:05:37 +0100

    [BUGFIX] drop support for 9.5
    di not working in symfony command controller

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '7.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.99.99',
+            'typo3' => '10.4.0-10.99.99',
         ],
         'conflicts' => [
         ],
