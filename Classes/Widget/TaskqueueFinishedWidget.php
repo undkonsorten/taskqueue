f53a9fc37bf9e636f764ce601419461caff0df05
Eike Starkmann <eike.starkmann@posteo.de>
2021-11-19 09:51:53 +0100

    [TASK] add widget for dashboard

---
+++ Classes/Widget/TaskqueueFinishedWidget.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types=1);
+
+namespace Undkonsorten\Taskqueue\Widget;
+
+use FriendsOfTYPO3\Dashboard\Widgets\AbstractLineChartWidget;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+
+class TaskqueueFinishedWidget extends AbstractLineChartWidget
+{
+    /**
+     * @var string
+     */
+    protected $title = 'Taskqueue finished fasks';
+
+    /**
+     * @var int
+     */
+    protected $width = 4;
+
+    /**
+     * @var int
+     */
+    protected $height = 4;
+
+    public function prepareData(): void
+    {
+    }
+
+    /**
+     *
+     */
+    protected function prepareChartData(): void
+    {
+        parent::prepareChartData();
+
+        $this->chartData = $this->getChartData();
+    }
+
+    /**
+     * @return array
+     */
+    protected function getChartData(): array
+    {
+        $period = 'lastMonth';
+
+        $labels = [];
+        $data = [];
+
+        if ($period === 'lastWeek') {
+            for ($daysBefore=7; $daysBefore--; $daysBefore>0) {
+                $labels[] = date('d-m-Y', strtotime('-' . $daysBefore . ' day'));
+                $startPeriod = strtotime('-' . $daysBefore . ' day 0:00:00');
+                $endPeriod =  strtotime('-' . $daysBefore . ' day 23:59:59');
+
+                $data[] = $this->getNumberOfErrorsInPeriod($startPeriod, $endPeriod);
+            }
+        }
+
+        if ($period === 'lastMonth') {
+            for ($daysBefore=31; $daysBefore--; $daysBefore>0) {
+                $labels[] = date('d-m-Y', strtotime('-' . $daysBefore . ' day'));
+                $startPeriod = strtotime('-' . $daysBefore . ' day 0:00:00');
+                $endPeriod =  strtotime('-' . $daysBefore . ' day 23:59:59');
+
+                $data[] = $this->getNumberOfErrorsInPeriod($startPeriod, $endPeriod);
+            }
+        }
+
+        return [
+            'labels' => $labels,
+            'datasets' => [
+                [
+                    'label' => $this->getLanguageService()->sL($this->languagePrefix . 'widgets.sysLogErrors.chart.dataSet.0'),
+                    'borderColor' => $this->chartColors[0],
+                    'fill' => false,
+                    'data' => $data
+                ]
+            ]
+        ];
+    }
+
+    protected function getNumberOfErrorsInPeriod(int $start, int $end): int
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_taskqueue_domain_model_task');
+        return (int)$queryBuilder
+            ->select('*')
+            ->from('tx_taskqueue_domain_model_task')
+            ->where(
+                $queryBuilder->expr()->eq('status', 2),
+                $queryBuilder->expr()->gte('tstamp', $start),
+                $queryBuilder->expr()->lte('tstamp', $end)
+            )
+            ->execute()
+            ->rowCount();
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,3 +5,10 @@ if (!defined('TYPO3_MODE')) {
 if (TYPO3_MODE === 'BE') {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = \Undkonsorten\Taskqueue\Controller\TaskQueueCommandController::class;
 }
+if(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('dashboard')){
+    $widgetRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\FriendsOfTYPO3\Dashboard\Registry\WidgetRegistry::class);
+    $widgetRegistry->registerWidget('taskqueue_finished', \Undkonsorten\Taskqueue\Widget\TaskqueueFinishedWidget::class);
+    $widgetRegistry->registerWidget('taskqueue_failed', \Undkonsorten\Taskqueue\Widget\TaskqueueFailedWidget::class);
+}
+
+
