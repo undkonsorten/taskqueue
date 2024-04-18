7147861a4d4f5fdbb2b6867527803cd4b361ab74
Karsten Nowak <nowak@undkonsorten.com>
2024-04-18 14:33:36 +0200

    [BUGFIX] Fix error with wrong declaration of execute method
    Fix compatibility with symfony/console for commands that extends
    symfony command.
    
    Related: #25577

--- Classes/Command/NotifyOnFailureCommand.php
+++ Classes/Command/NotifyOnFailureCommand.php
@@ -51,7 +51,7 @@ class NotifyOnFailureCommand extends Command
 
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
         $io->title($this->getDescription());
