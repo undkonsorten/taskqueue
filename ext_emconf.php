bf85c4530a30727a5344b5b9abe3fd554d63df52
Karsten Nowak <nowak@undkonsorten.com>
2025-05-20 22:22:33 +0200

    [TASK] Remove TYPO3 12 dependecny in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '9.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.99.99',
+            'typo3' => '13.0.0-13.99.99',
         ],
         'conflicts' => [
         ],
