3fe1ab4a2e8df5dff437aaa9f29375a568c75605
jalr <mail@jalr.de>
2026-04-27 10:34:54 +0200

    Remove registerModule call
    This function was removed in TYPO3 version 11.

--- ext_tables.php
+++ ext_tables.php
@@ -2,27 +2,3 @@
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
-(function () {
-    if (TYPO3 === 'BE') {
-
-        /**
-         * Registers a Backend Module
-         */
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-            'Taskqueue',
-            'tools',     // Make module a submodule of 'tools'
-            'taskqueue',    // Submodule key
-            '',                        // Position
-            [
-                \Undkonsorten\Taskqueue\Controller\TaskController::class => 'list, show, delete, run, deleteFinished, deleteFailed, reactivate, search, searchUid, searchResult',
-
-            ],
-            [
-                'access' => 'user,group',
-                'icon' => 'EXT:taskqueue/Resources/Public/Icons/Extension.svg',
-                'labels' => 'LLL:EXT:taskqueue/Resources/Private/Language/locallang_taskqueue.xlf',
-            ]
-        );
-    }
-
-})();
